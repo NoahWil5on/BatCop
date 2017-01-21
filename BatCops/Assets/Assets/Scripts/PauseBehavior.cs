84c343c06da0537af03460cb195ca00de4fb59c8
MAIN\ntw9049 <ntw9049@g.rit.edu>
2017-01-21 14:21:56 -0500

    Fixed Sonar, Further implented pause menu

---
+++ BatCops/Assets/Assets/Scripts/PauseBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseBehavior : MonoBehaviour {
+	public GameObject canvas;
+	// Use this for initialization
+	void Start () {
+		canvas.SetActive(false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			if(canvas.activeInHierarchy){
+				canvas.SetActive(false);
+			}
+			else{
+				canvas.SetActive(true);
+			}
+		}
+	}
+}
