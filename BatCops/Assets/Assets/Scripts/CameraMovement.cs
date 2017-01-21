fc996bc7f85b76c8e6b39d51537ba56b85ebad5b
MAIN\ntw9049 <ntw9049@g.rit.edu>
2017-01-21 13:23:13 -0500

    All files from old repository

---
+++ BatCops/Assets/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	Vector3 position;
+
+	public GameObject bat;
+	public float range;
+
+
+	// Use this for initialization
+	void Start () {
+		position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			MoveTowards();
+		
+	}
+
+	void MoveTowards(){
+		Vector3 goal = Vector3.Lerp(transform.position,bat.transform.position,.1f);
+		transform.position = new Vector3(goal.x,position.y,goal.z);
+	}
+
+	float DistanceSqr(Vector3 target){
+		return Mathf.Pow(position.x-target.x,2) + Mathf.Pow(position.z-target.z,2);
+	}
+}
---
+++ BatCops/Assets/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    //------------------------------------------------------------------------------>>FIELDS<<
+    // Robber Rat
+    public GameObject robberRat;
+
+    // Array of all exit points the Robber Thief is going for
+    private List<GameObject> exitPoints;
+
+    // Keeps track of Vehicle class for Robber Rat
+    private Wander robberWander;
+    private int find;
+
+    //------------------------------------------------------------------------------>>CONSTRUCTOR<<
+    // Use this for initialization
+    void Start () {
+        // Adds all exit points to a private array list - used for Rat Thief's pathfinding
+        exitPoints = new List<GameObject>();
+        foreach(GameObject g in GameObject.FindGameObjectsWithTag("ExitPoint"))
+        {
+            exitPoints.Add(g);
+        }
+
+        find = 0;
+
+        robberWander = robberRat.GetComponent<Wander>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float dist = Vector3.Distance(robberRat.transform.position, exitPoints[find].transform.position);
+        if(find > exitPoints.Count - 1)
+        {
+            find = 0;
+        }
+
+    }
+}
